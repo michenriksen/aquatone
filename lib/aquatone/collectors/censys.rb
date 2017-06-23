c8ed531ab2f69b969a0eb9e9ec84b542093296f4
James McLean <james.mclean@gmail.com>
2017-06-23 22:48:26 +0930

    Communicating with Censys API correctly. Now to parse results

--- lib/aquatone/collectors/censys.rb
+++ lib/aquatone/collectors/censys.rb
@@ -3,10 +3,9 @@ module Aquatone
     class Censys < Aquatone::Collector
       self.meta = {
         :name         => "Censys",
-        :author       => "James McLean (vortexau)",
+        :author       => "James McLean (@vortexau)",
         :description  => "Uses the Censys API to find hostnames in TLS certificates",
-        :require_keys => ["censys"]
-        :require_id   => ["censys"]
+        :require_keys => ["censys","censysid"],
       }
 
       API_BASE_URI         = "https://www.censys.io/api/v1".freeze
@@ -17,11 +16,46 @@ module Aquatone
         request_censys_page
       end
 
-      private
+      def request_censys_page(page=1)
+          # Initial version only supporting Censys Certificates API
 
-      def request_censys_page()
+          # Censys expects Basic Auth for requests.
+          auth = {
+              :username => get_key('censysid'), 
+              :password => get_key('censys')
+          }
+   
+          # Define this is JSON content
+          headers = {
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+          }
 
+          # The post body itself, as JSON
+          query = {
+              'query'   => url_escape("#{domain.name}"),
+              'page'    => page,
+              'fields'  => [ "parsed.names", "parsed.extensions.subject_alt_name.dns_names" ],
+              'flatten' => true
+          }
 
+          # Search API documented at https://censys.io/api/v1/docs/search
+          response = post_request(
+              "#{API_BASE_URI}/search/certificates", 
+              query.to_json,
+              {
+                  :basic_auth => auth,
+                  :headers => headers 
+              }
+          )
+
+          if response.code != 200
+              failure(response.parsed_response["error"] || "Censys API encountered error: #{response.code}")
+          end
+
+          # Parse the actual response here.
+          
+          print response
 
       end
     end
