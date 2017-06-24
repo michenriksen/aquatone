a0a4d4b8e6a172cb01faf9a6bdd58dd812bc7c89
jolle <jolle@users.noreply.github.com>
2017-06-24 18:04:56 +0300

    Escape domain name in regex

--- lib/aquatone/collectors/crtsh.rb
+++ lib/aquatone/collectors/crtsh.rb
@@ -10,7 +10,7 @@ module Aquatone
       def run
         response = get_request("https://crt.sh/?dNSName=%25.#{url_escape(domain.name)}")
 
-        response.body.to_enum(:scan, /<TD>([a-zA-Z0-9\*_.-]+\.#{domain.name})<\/TD>/).map do |column|
+        response.body.to_enum(:scan, /<TD>([a-zA-Z0-9\*_.-]+\.#{Regexp.escape(domain.name)})<\/TD>/).map do |column|
           add_host(column[0].gsub("*.", ""))
         end
       end
