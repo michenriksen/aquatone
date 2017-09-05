9a1bb4c0cd9e88659221818aa16ef40ec9072335
Rami McCarthy <rami@ramimac.me>
2017-09-05 17:07:23 -0400

    Remove fastly false positive

--- lib/aquatone/detectors/fastly.rb
+++ lib/aquatone/detectors/fastly.rb
@@ -9,12 +9,15 @@ module Aquatone
       }
 
       CNAME_VALUE          = ".fastly.net".freeze
+      CNAME_FALSE_POSITIVE = ".map.fastly.net".freeze
       RESPONSE_FINGERPRINT = "Fastly error: unknown domain".freeze
 
       def run
         return false unless cname_resource?
         if resource_value.end_with?(CNAME_VALUE)
-          return get_request("http://#{host}/").body.include?(RESPONSE_FINGERPRINT)
+          unless resource_value.end_with?(CNAME_FALSE_POSITIVE)
+            return get_request("http://#{host}/").body.include?(RESPONSE_FINGERPRINT)
+          end
         end
         false
       end
