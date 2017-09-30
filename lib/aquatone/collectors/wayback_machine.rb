afa3b04af875a815cd736979c375e89082bb5aa1
Michael Henriksen <michenriksen@neomailbox.ch>
2017-09-30 10:42:32 +0200

    Implement configurable timeout option to Wayback Machine collector

--- lib/aquatone/collectors/wayback_machine.rb
+++ lib/aquatone/collectors/wayback_machine.rb
@@ -6,17 +6,33 @@ module Aquatone
       self.meta = {
         :name         => "Wayback Machine",
         :author       => "Joel (@jolle)",
-        :description  => "Uses Wayback Machine by Internet Archive to find unique hostnames"
+        :description  => "Uses Wayback Machine by Internet Archive to find unique hostnames",
+        :cli_options  => {
+          "wayback-machine-timeout SECONDS" => "Timeout for Wayback Machine collector in seconds (default: 30)"
+        }
       }
 
-      def run
-        response = get_request("http://web.archive.org/cdx/search/cdx?url=*.#{url_escape(domain.name)}&output=json&fl=original&collapse=urlkey")
+      DEFAULT_TIMEOUT = 30.freeze
 
+      def run
+        response = nil
+        Timeout::timeout(timeout) do
+          response = get_request("http://web.archive.org/cdx/search/cdx?url=*.#{url_escape(domain.name)}&output=json&fl=original&collapse=urlkey")
+        end
         response.parsed_response.each do |page|
           if page[0] != "original"
               add_host(URI.parse(page[0]).host)
           end
         end
+      end
+
+      private
+
+      def timeout
+        if has_cli_option?("wayback-machine-timeout")
+          return get_cli_option("wayback-machine-timeout").to_i
+        end
+        DEFAULT_TIMEOUT
       end
     end
   end
