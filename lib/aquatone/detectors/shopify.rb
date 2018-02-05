2c7dd4803cf46cc1784c6878edd105c991d02dd2
europa <eur0pa@users.noreply.github.com>
2018-02-05 11:30:31 +0100

    new detectors, some fixes

--- lib/aquatone/detectors/shopify.rb
+++ lib/aquatone/detectors/shopify.rb
@@ -3,20 +3,22 @@ module Aquatone
     class Shopify < Aquatone::Detector
       self.meta = {
         :service         => "Shopify",
-        :service_website => "https://shopify.com/",
-        :author          => "Alessandro De Micheli (@eur0pa_)",
-        :description     => "Shop hosting"
+        :service_website => "https://www.shopify.com/",
+        :author          => "Michael Henriksen (@michenriksen)",
+        :description     => "Ecommerce platform"
       }
 
-      CNAME_VALUE          = ".myshopify.com".freeze
-      RESPONSE_FINGERPRINT = "Sorry, this shop is currently unavailable".freeze
+      APEX_VALUE           = "23.227.38.32".freeze
+      CNAME_VALUE          = "shops.myshopify.com".freeze
+      RESPONSE_FINGERPRINT = "Sorry, this shop is currently unavailable.".freeze
 
       def run
-        return false unless cname_resource?
-        if resource_value.end_with?(CNAME_VALUE)
-          return get_request("http://#{host}/").body.include?(RESPONSE_FINGERPRINT)
+        if apex_resource?
+          return false unless resource_value == APEX_VALUE
+        elsif cname_resource?
+          return false unless resource_value == CNAME_VALUE
         end
-        false
+        get_request("http://#{host}/").body.include?(RESPONSE_FINGERPRINT)
       end
     end
   end
