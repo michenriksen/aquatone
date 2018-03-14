d984456b286bdbb14d9986d6cd6d18ebd9b7e037
Duarte Duarte <dnpd.dd@gmail.com>
2018-03-14 11:32:08 +0000

    Add subnets for Squarespace IPs

--- lib/aquatone/detectors/squarespace.rb
+++ lib/aquatone/detectors/squarespace.rb
@@ -1,3 +1,5 @@
+require "ipaddr"
+
 module Aquatone
   module Detectors
     class Squarespace < Aquatone::Detector
@@ -9,8 +11,11 @@ module Aquatone
       }
 
       RESPONSE_FINGERPRINT = "Squarespace - Claim This Domain".freeze
+      APEX_VALUES          = [IPAddr.new("198.185.159.0/24"), IPAddr.new("198.49.23.0/24")].freeze
 
       def run
+        if apex_resource?
+          return false unless APEX_VALUES.include?(IPAddr.new(resource_value)))
         return get_request("http://#{host}/").body.include?(RESPONSE_FINGERPRINT)
       end
     end
