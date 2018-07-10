3b89522acac88f40190256ac945558eac4af4204
Shawn May <shawnmay@me.com>
2018-07-10 02:07:36 -0700

    Fixing PTRArchive (with updated URL)
    Note: This commit resolves an issue with the PTRArchive collector.
    There is another pull request that exists for this issue, but I believe
    it’s hitting a URL that’s since been changed.
    
    However, on a larger scale, the owner of PTRArchive seems to not be a
    huge fan of tools querying the data, and seems to be indiscriminately
    labeling all applications (even those with legitimate utility for
    penetration testers) 'black hat tools'. In the case of Sublist3r, this
    source was removed as a source after tampering with returned data or
    blocking wholesale all requests. I believe this may actually be why the
    URL has shifted between my pull request and the previous. Perhaps this
    collector should be removed?

--- lib/aquatone/collectors/ptrarchive.rb
+++ lib/aquatone/collectors/ptrarchive.rb
@@ -8,7 +8,11 @@ module Aquatone
       }
 
       def run
-        response = get_request("http://ptrarchive.com/tools/search.htm?label=#{url_escape(domain.name)}&date=ALL")
+        response = get_request("http://ptrarchive.com/tools/search3.htm?label=#{url_escape(domain.name)}&date=ALL",
+         :headers => {
+           "User-Agent" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.3) Gecko/20100402 Prism/1.0b4"
+         }
+        )
         if response.code != 200
           failure("PTRArchive returned unexpected response code: #{response.code}")
         end
