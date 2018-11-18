cf4209ee8f7dddd88eb6ec7454deacffa1fe03ef
europa <eur0pa@users.noreply.github.com>
2018-11-18 10:56:20 +0100

    use chromedp instead - thanks to mzack9999

--- agents/url_screenshotter.go
+++ agents/url_screenshotter.go
@@ -3,17 +3,25 @@ package agents
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/cdproto/security"
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/runner"
 	"github.com/michenriksen/aquatone/core"
 )
 
 type URLScreenshotter struct {
 	session    *core.Session
 	chromePath string
+	ctxt       context.Context
+	pool       *chromedp.Pool
 }
 
 func NewURLScreenshotter() *URLScreenshotter {
@@ -28,6 +36,8 @@ func (a *URLScreenshotter) Register(s *core.Session) error {
 	s.EventBus.SubscribeAsync(core.URLResponsive, a.OnURLResponsive, false)
 	a.session = s
 	a.locateChrome()
+	a.ctxt, _ = context.WithCancel(context.Background())
+	a.pool, _ = chromedp.NewPool()
 
 	return nil
 }
@@ -80,47 +90,89 @@ func (a *URLScreenshotter) locateChrome() {
 
 func (a *URLScreenshotter) screenshotURL(s string) {
 	filePath := a.session.GetFilePath(fmt.Sprintf("screenshots/%s.png", BaseFilenameFromURL(s)))
-	var chromeArguments = []string{
-		"--headless", "--disable-gpu", "--hide-scrollbars", "--mute-audio", "--disable-notifications",
-		"--disable-crash-reporter",
-		"--ignore-certificate-errors",
-		"--user-agent=" + RandomUserAgent(),
-		"--window-size=" + *a.session.Options.Resolution,
-		"--screenshot=" + filePath,
+
+	// allocate a chrome headless instance
+	c, err := a.pool.Allocate(a.ctxt,
+		runner.DisableGPU,
+		runner.Headless,
+		runner.ExecPath(a.chromePath),
+		runner.Flag("ignore-certificate-errors", true),
+		runner.Flag("disable-crash-reporter", true),
+		runner.Flag("disable-notifications", true),
+		runner.Flag("hide-scrollbars", true),
+		runner.Flag("window-size", *a.session.Options.Resolution),
+		runner.Flag("user-agent", RandomUserAgent()),
+		runner.Flag("mute-audio", true),
+		runner.Flag("incognito", true),
+	)
+	defer c.Release()
+
+	// screenshot buffer
+	var picbuf []byte
+
+	// set headers
+	ip := RandomIPv4Address()
+	if *a.session.Options.Waf {
+		ip = "127.0.0.1"
+	}
+	headers := map[string]interface{}{
+		"X-Client-IP":     ip,
+		"X-Remote-IP":     ip,
+		"X-Remote-Addr":   ip,
+		"X-Forwarded-For": ip,
+		"X-OriginatingIP": ip,
+		"Via":             "1.1 " + ip,
+		"Forwarded":       "for=" + ip + ";proto=http;by=" + ip,
 	}
 
-	if os.Geteuid() == 0 {
-		chromeArguments = append(chromeArguments, "--no-sandbox")
+	// create a new tab and snap it
+	t := chromedp.Tasks{
+		network.Enable(),
+		network.SetExtraHTTPHeaders(network.Headers(headers)),
+		security.SetIgnoreCertificateErrors(true),
+		chromedp.Navigate(s),
+		chromedp.Sleep(time.Second * 5),
+		chromedp.ActionFunc(func(ctxt context.Context, h cdp.Executor) error {
+			picbuf, err = page.CaptureScreenshot().Do(ctxt, h)
+			if err != nil {
+				a.session.Out.Debug("[%s] Error: %v\n", a.ID(), err)
+				a.session.Stats.IncrementScreenshotFailed()
+				a.session.Out.Error("%s: screenshot failed: %s\n", s, err)
+				return err
+			}
+			return nil
+		}),
 	}
 
-	if *a.session.Options.Proxy != "" {
-		chromeArguments = append(chromeArguments, "--proxy-server="+*a.session.Options.Proxy)
-	}
-
-	chromeArguments = append(chromeArguments, s)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*a.session.Options.ScreenshotTimeout)*time.Millisecond)
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, a.chromePath, chromeArguments...)
-	if err := cmd.Start(); err != nil {
+	// run tasks
+	err = c.Run(a.ctxt, t)
+	if err != nil {
 		a.session.Out.Debug("[%s] Error: %v\n", a.ID(), err)
 		a.session.Stats.IncrementScreenshotFailed()
 		a.session.Out.Error("%s: screenshot failed: %s\n", s, err)
 		return
 	}
 
-	if err := cmd.Wait(); err != nil {
-		a.session.Stats.IncrementScreenshotFailed()
+	// write to disk
+	err = ioutil.WriteFile(filePath, picbuf, 0644)
+	if err != nil {
 		a.session.Out.Debug("[%s] Error: %v\n", a.ID(), err)
-		if ctx.Err() == context.DeadlineExceeded {
-			a.session.Out.Error("%s: screenshot timed out\n", s)
-			return
+		a.session.Stats.IncrementScreenshotFailed()
+		a.session.Out.Error("%s: screenshot failed: %s\n", s, err)
+		return
+	}
+
+	/*
+		if os.Geteuid() == 0 {
+			chromeArguments = append(chromeArguments, "--no-sandbox")
 		}
 
-		a.session.Out.Error("%s: screenshot failed: %s\n", s, err)
-		return
-	}
+		if *a.session.Options.Proxy != "" {
+			chromeArguments = append(chromeArguments, "--proxy-server="+*a.session.Options.Proxy)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*a.session.Options.ScreenshotTimeout)*time.Millisecond)
+	*/
 
 	a.session.Stats.IncrementScreenshotSuccessful()
 	a.session.Out.Info("%s: %s\n", s, Green("screenshot successful"))
