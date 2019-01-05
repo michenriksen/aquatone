517488da695749c8a99e1bbb81aa6223a9f3d632
Michael Henriksen <mchnrksn@gmail.com>
2019-01-05 13:17:09 +0100

    Attempt to detect TLS on non-standard ports in URL Publisher agent

--- agents/url_publisher.go
+++ agents/url_publisher.go
@@ -1,6 +1,11 @@
 package agents
 
 import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"time"
+
 	"github.com/michenriksen/aquatone/core"
 )
 
@@ -24,6 +29,32 @@ func (a *URLPublisher) Register(s *core.Session) error {
 
 func (a *URLPublisher) OnTCPPort(port int, host string) {
 	a.session.Out.Debug("[%s] Received new open port on %s: %d\n", a.ID(), host, port)
-	url := HostAndPortToURL(host, port, "")
+	var url string
+	if a.isTLS(port, host) {
+		url = HostAndPortToURL(host, port, "https")
+	} else {
+		url = HostAndPortToURL(host, port, "http")
+	}
 	a.session.EventBus.Publish(core.URL, url)
+}
+
+func (a *URLPublisher) isTLS(port int, host string) bool {
+	if port == 80 {
+		return false
+	}
+
+	if port == 443 {
+		return true
+	}
+
+	dialer := &net.Dialer{Timeout: time.Duration(*a.session.Options.HTTPTimeout) * time.Millisecond}
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	conn, err := tls.DialWithDialer(dialer, "tcp", fmt.Sprintf("%s:%d", host, port), conf)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
 }
