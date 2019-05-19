854a5d56fbb7a00b2e5ec80d443026c7a4ced798
Michael Henriksen <mchnrksn@gmail.com>
2019-05-19 11:07:52 +0200

    Releasing v1.7.0

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "aquatone"
-	Version = "1.7.0-beta.2"
+	Version = "1.7.0"
 	Author  = "Michael Henriksen"
 	Website = "https://github.com/michenriksen/aquatone"
 )
