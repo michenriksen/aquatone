9bf8ee0c889b1072df6ae9483004844b94817e94
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-05-01 14:20:01 +0700

    Better config output

--- main.go
+++ main.go
@@ -145,10 +145,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	sess.Out.Important("Targets    : %d\n", len(targets))
-	sess.Out.Important("Threads    : %d\n", *sess.Options.Threads)
-	sess.Out.Important("Ports      : %s\n", strings.Trim(strings.Replace(fmt.Sprint(sess.Ports), " ", ", ", -1), "[]"))
-	sess.Out.Important("Output dir : %s\n\n", *sess.Options.OutDir)
+	sess.Out.Important("==============================\n")
+	sess.Out.Important("- Targets          : %d\n", len(targets))
+	sess.Out.Important("- Threads          : %d\n", *sess.Options.Threads)
+	sess.Out.Important("- Ports            : %s\n", strings.Trim(strings.Replace(fmt.Sprint(sess.Ports), " ", ", ", -1), "[]"))
+	sess.Out.Important("- Output Directory : %s\n", *sess.Options.OutDir)
+	sess.Out.Important("==============================\n\n")
 
 	sess.EventBus.Publish(core.SessionStart)
 
