45489f03db19821b79147f939e897637605d36d2
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-05-01 14:11:22 +0700

    Increase the scan-timeout

--- core/options.go
+++ core/options.go
@@ -35,7 +35,7 @@ func ParseOptions() (Options, error) {
 		ChromePath:        flag.String("chrome-path", "", "Full path to the Chrome/Chromium executable to use. By default, aquatone will search for Chrome or Chromium"),
 		Resolution:        flag.String("resolution", "1440,900", "screenshot resolution"),
 		Ports:             flag.String("ports", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(MediumPortList)), ","), "[]"), "Ports to scan on hosts. Supported list aliases: small, medium, large, xlarge"),
-		ScanTimeout:       flag.Int("scan-timeout", 100, "Timeout in miliseconds for port scans"),
+		ScanTimeout:       flag.Int("scan-timeout", 2*1000, "Timeout in miliseconds for port scans"),
 		HTTPTimeout:       flag.Int("http-timeout", 3*1000, "Timeout in miliseconds for HTTP requests"),
 		ScreenshotTimeout: flag.Int("screenshot-timeout", 30*1000, "Timeout in miliseconds for screenshots"),
 		Nmap:              flag.Bool("nmap", false, "Parse input as Nmap/Masscan XML"),
